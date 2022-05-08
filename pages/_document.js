7575394e2e95dd183759304aa3ac66bafb2c67b9
christian-toscano <toscano@extendi.it>
2022-05-08 19:21:03 +0200

    adds faenz analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -7,6 +8,11 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css?family=Roboto%20Slab:400&display=swap"
           rel="stylesheet"
+        />
+        <Script
+          defer
+          data-domain="markdown-2-medium.vercel.app"
+          src="https://faenz.onrender.com/faenz.js"
         />
       </Head>
       <body>
